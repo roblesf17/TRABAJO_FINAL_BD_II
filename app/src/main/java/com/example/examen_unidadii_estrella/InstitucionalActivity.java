6ee6e188f150c0de57e305dc0b4fa026c3ab11f0
roblesf17 <roblesf17@gmail.com>
2019-12-10 22:52:56 -0500

    actualizacion proyecto boot

--- app/src/main/java/com/example/examen_unidadii_estrella/InstitucionalActivity.java
+++ app/src/main/java/com/example/examen_unidadii_estrella/InstitucionalActivity.java
@@ -11,11 +11,31 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class InstitucionalActivity extends AppCompatActivity {
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class InstitucionalActivity extends AppCompatActivity implements AIListener {
+
+
+
+
+    //INICIO DIALOG FLOW DIALOGFLOW -----------------
+    private TextToSpeech mTextToSpeech;
+    private AIService mAIService;
+    //FIN DIALOG FLOW ------------------------------
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,12 +93,68 @@ public class InstitucionalActivity extends AppCompatActivity {
             }
         });
 
+
+
+        //INICIO DIALOFLOW -------------------------
+        final ai.api.android.AIConfiguration config = new ai.api.android.AIConfiguration
+                ("aede6a2870454455a5d45d718555e9dd", ai.api.android.AIConfiguration.SupportedLanguages.Spanish, AIConfiguration.RecognitionEngine.System);
+        mAIService = AIService.getService(this, config);
+        mAIService.setListener( this );
+        mTextToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+            }
+        });
+        //FIN DIALOGFLOW ----------------------------
+
+
+    }
+
+
+    //INICIO DIALOG FLOW --------------------------------------
+    public void AsistenteBoot(View view)
+    {
+        mAIService.startListening();
     }
 
 
 
 
 
+
+    @Override
+    public void onError(AIError error)
+    {
+
+    }
+
+    @Override
+    public void onResult(AIResponse response) {
+        Result result = response.getResult();
+        mTextToSpeech.speak(result.getFulfillment().getSpeech(), TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
+    }
+    //FIN DIALOG FLOW -------------------------------------------
 
 
 
